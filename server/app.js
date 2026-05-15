ca7310292809005e4cdba8422a58f7210198d3a9
Sinan027 <sinan202408@gmail.com>
2026-05-15 09:38:18 +0530

    aa

--- server/app.js
+++ server/app.js
@@ -75,8 +75,6 @@ app.use("/api/bmi", bmiRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV !== "production") {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
